ff46dd9cbef359ca7336dce9ccfa80fa5f4fbd6e
Harman1105 <harman.meet.2005@gmail.com>
2026-09-01 23:48:16 +0530

    fixed analytic route imports

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getAnalytics } from "../controllers/analyticsController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
